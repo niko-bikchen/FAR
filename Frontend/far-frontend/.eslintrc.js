73cae253569f49d777cfe76baf26b1e9f084d002
Nikolay Bikchentaev <35120296+niko-bikchen@users.noreply.github.com>
2019-09-30 01:25:17 +0300

    Modified ESLint config file and made some methods for Converter component
    • ESLint now requires double quotes and no dangling commas
    • "Switch" button now works
    • "Convert" button invokes method which looks for numbers in input text

--- Frontend/far-frontend/.eslintrc.js
+++ Frontend/far-frontend/.eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ["error", "never"],
+    quotes: ["error", "double"]
   },
   parserOptions: {
     parser: 'babel-eslint',
--- Frontend/far-frontend/src/components/Converter.vue
+++ Frontend/far-frontend/src/components/Converter.vue
@@ -2,28 +2,36 @@
   <div class="converter-body col-md-12">
     <div class="row text-center converter-body__contents">
       <div class="col-md-12 col-lg-6 pr-2 converter-body__textarea-source">
-        <h3 class="converter-body__textarea-heading">ARABIC</h3>
+        <h3 class="converter-body__textarea-heading">{{ from }}</h3>
         <textarea
-          name="source-text"
+          name="source-textarea"
           class="converter-body__textarea"
           @focus="sourceChosen = true"
           @blur="sourceChosen = false"
           :class="{highlighted: sourceChosen}"
+          ref="source"
         ></textarea>
       </div>
-      <div class="col-md-12 col-lg-6 pl-2 converter-body__textarea-destination">
-        <h3 class="converter-body__textarea-heading">ROMAN</h3>
+      <div class="col-md-12 col-lg-6 pl-2 converter-body__textarea-target">
+        <h3 class="converter-body__textarea-heading">{{ to }}</h3>
         <textarea
-          name="source-text"
+          name="target-textarea"
           class="converter-body__textarea"
-          @focus="destinationChosen = true"
-          @blur="destinationChosen = false"
-          :class="{highlighted: destinationChosen}"
+          @focus="targetChosen = true"
+          @blur="targetChosen = false"
+          :class="{highlighted: targetChosen}"
+          ref="target"
         ></textarea>
       </div>
       <div class="col-md-12 col-lg-6 offset-lg-3">
-        <button class="btn btn-block btn-primary converter-body__convert-btn">CONVERT</button>
-        <button class="btn btn-block btn-primary converter-body__convert-btn">SWITCH</button>
+        <button
+          class="btn btn-block btn-primary converter-body__convert-btn"
+          @click="processInput"
+        >CONVERT</button>
+        <button
+          class="btn btn-block btn-primary converter-body__convert-btn"
+          @click="switchFrom"
+        >SWITCH</button>
       </div>
     </div>
   </div>
@@ -34,8 +42,32 @@ export default {
   data() {
     return {
       sourceChosen: false,
-      destinationChosen: false
+      targetChosen: false,
+      to: "ROMAN",
+      from: "ARABIC"
     };
+  },
+  methods: {
+    switchFrom() {
+      if (this.from === "ARABIC") {
+        this.from = "ROMAN";
+        this.to = "ARABIC";
+      } else if (this.from === "ROMAN") {
+        this.from = "ARABIC";
+        this.to = "ROMAN";
+      }
+    },
+    processInput() {
+      // TODO: Make this method more secure
+      var sourceText = this.$refs.source.value;
+      var numbers = sourceText.match(/\d+/gm);
+      numbers.forEach(element => {
+        sourceText = sourceText.replace(element, `<number>${element}</number>`);
+      });
+
+      console.log(sourceText);
+      console.log(numbers);
+    }
   }
 };
 </script>
@@ -51,6 +83,10 @@ $mainColor: rgba(0, 57, 108, 1);
   &__textarea {
     width: 100%;
     height: 300px;
+
+    padding: 15px;
+
+    font-size: 18px;
 
     resize: none;
 
@@ -76,7 +112,7 @@ $mainColor: rgba(0, 57, 108, 1);
     }
   }
 
-  &__textarea-destination {
+  &__textarea-target {
     textarea {
       border: 1px solid $mainColor;
     }
